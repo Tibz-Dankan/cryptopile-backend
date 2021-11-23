d9d98699193a5f27542186d7152a0a6907b06ecc
Tibz-Dankan <tibesigwadankan@gmail.com>
2021-11-23 17:09:10 +0300

    ampersand, curly braces and paremthesis removed from verifyEmail.js

--- verifyEmail.js
+++ verifyEmail.js
@@ -1,35 +1,39 @@
+const express = require("express");
 const nodemailer = require("nodemailer");
+const app = express();
+require("dotenv").config();
 
 // function send verification link to user's email
-const sendVerificationEmail = (userEmail, userVerificationId) => {
+const sendVerificationEmail = (userEmail) => {
+  let userVerificationId = math.floor(math.random() * 100 + 45);
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
       user: "tibesigwadankan@gmail.com",
-      pass: "tibs@fe20",
+      pass: "my-password",
     },
   });
-  // <a href="https://stockpile-frontend.netlify.app/verify-email">here</a>
   // mail options
   const mailOptions = {
     from: "tibesigwadankan@gmail.com",
     to: `${userEmail}`,
     subject: "Email Confirmation",
-    html: `Your verification code  is <h1> ${userVerificationId} </h1>`,
+    html: `Please click  
+      <a href="https://cryptopile.netlify.app/verify-email">here</a> to verify email,`,
   };
   // send email
   transporter.sendMail(mailOptions, (error, data) => {
     if (error) {
       console.log(error);
-      // res.send(error);
+      res.send(error);
     } else {
       console.log("Message sent");
-      // console.log(data.response);
-      // res.send(data.response);
+      console.log(data.response);
+      res.send(data.response);
     }
   });
 };
 
 // route to verify the user email
 // continue reasoning from here next time
-module.exports = { sendVerificationEmail };
+(module.exports = app), { sendVerificationEmail };
