71f8ac180bcc0c20572d6bacf84d44de14bd8d63
Tibz-Dankan <tibesigwadankan@gmail.com>
2021-12-27 23:56:34 +0300

    resetPassword.js file created

---
+++ resetPassword.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require("cors");
+const bcrypt = require("bcrypt");
+const pool = require("./dbConfig");
+const app = express();
+app.use(express.json());
+// app.use(cors({ origin: "https://cryptopile.netlify.app" }));
+app.use(cors());
+
+// sending verification code to user email
+app.post("/reset-password/:userId", async (req, res) => {
+  try {
+    const { userEmail } = req.body;
+    const { newPassword } = req.body;
+    const { userId } = req.params;
+    const newHashedPassword = await bcrypt.hash(newPassword, 10);
+    const sqlQuery =
+      "UPDATE registers SET password = $1 WHERE id = $2 AND email =$3 RETURNING *";
+    await pool.connect();
+    const response = await pool.query(sqlQuery, [
+      newHashedPassword,
+      userId,
+      userEmail,
+    ]);
+    if (response.rows.length > 0) {
+      console.log("Password reset to: " + response.rows[0].password);
+      res.send({ passwordResetMsg: "password-reset-successful" });
+    } else {
+      res.send({ passwordResetMsg: "Can not reset password" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+// also check if changed if the password has changed from the one being stored in the database
+// and if yes proceed and changed if not, should not be changed
+module.exports = app;
