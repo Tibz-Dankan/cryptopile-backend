b2fbde791a71292de525c9504562ee5f4703b237
Tibz-Dankan <tibesigwadankan@gmail.com>
2021-12-30 02:56:03 +0300

    url in email verification link changed to the one in production

--- account/sendVerificationEmail.js
+++ account/sendVerificationEmail.js
@@ -19,10 +19,10 @@ const sendEmailVerificationLink = (userEmail, userId, verificationCode) => {
     from: "CryptoPile <cryptopile20@gmail.com>",
     to: `${userEmail}`,
     subject: "Email Confirmation",
-    html: `<h4> click the button below to confirm your email and complete the registration process <br/><br/>
-      <a href="http://localhost:3000/#/verify-user-email/?userId=${userId}&verificationCode=${verificationCode}"><button style="background-color:lightseagreen"> confirm email </button></a> <br/><br/> You received this email because you are signing up for <b> CryptoPile </b> </h4>`,
     // html: `<h4> click the button below to confirm your email and complete the registration process <br/><br/>
-    //   <a href="https://cryptopile.netlify.app/#/verify-user-email/?userId=${userId}&verificationCode=${verificationCode}"><button style="background-color:lightseagreen"> confirm email </button></a> <br/><br/> You received this email because you are signing up for <b> CryptoPile </b></h4>`,
+    //   <a href="http://localhost:3000/#/verify-user-email/?userId=${userId}&verificationCode=${verificationCode}"><button style="background-color:lightseagreen"> confirm email </button></a> <br/><br/> You received this email because you are signing up for <b> CryptoPile </b> </h4>`,
+    html: `<h4> click the button below to confirm your email and complete the registration process <br/><br/>
+      <a href="https://cryptopile.netlify.app/#/verify-user-email/?userId=${userId}&verificationCode=${verificationCode}"><button style="background-color:lightseagreen"> confirm email </button></a> <br/><br/> You received this email because you are signing up for <b> CryptoPile </b></h4>`,
   };
   // send email
   transporter.sendMail(mailOptions, (error, data) => {
