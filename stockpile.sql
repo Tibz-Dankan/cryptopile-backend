568c24436e6a3d4fd90dc953d53a4bcc93abd365
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-02-18 14:33:12 +0300

    unnecessary comments removed

--- stockpile.sql
+++ stockpile.sql
@@ -42,47 +42,6 @@ CREATE TABLE imageUrl (
      imageUrls VARCHAR(250) NOT NULL
 );
 
--- adding the title_iv column and description_iv column and even to the production databsase (heroku)
--- CREATE TABLE secretes (
--- secrete_id BIGSERIAL PRIMARY KEY,
--- user_id INT NOT NULL,
--- user_name VARCHAR(50) NOT NULL,
--- secrete_title VARCHAR(300) NOT NULL,
--- secrete_description VARCHAR(400) NOT NULL,
--- iv VARCHAR(250) NOT NULL
--- );
-
-
-
---    To add the columns named  date&time, updated ,detele to store the deleteled data from the pile table
-
--- ALTER TABLE pile ADD COLUMN date_of_add DATE;
--- ALTER TABLE pile ADD COLUMN date_of_add TIME ;     -- this is for trial purpoese
--- ALTER TABLE pile ADD COLUMN date_of_add TIMESTAMP ;     -- this is for trial purpoese
--- ALTER TABLE pile ADD COLUMN date_of_add TIMESTAMP '2004-10-19 10:23:54' ;     -- this is for trial purpose noy working
--- ALTER TABLE pile ADD COLUMN date_of_add DATE, TIME, TIMESTAMP ;     -- this is for trial purpose
--- ALTER TABLE pile ADD COLUMN time_of_add TIME; 
--- ALTER TABLE pile ADD COULUMN user_Id INT(10); -- added with lots of love
--- ALTER TABLE pile ADD COULUMN user_name VARCHAR(255);
-
--- When it comes to production time please remeber to add notnull constrants to 
--- the data time columns added ti the pile table 
-
-
--- -- DROPING THESE COLUMNS TO ADD MORE CONSTRAINTS
--- ALTER TABLE pile DROP date_of_add;
--- ALTER TABLE pile DROP time_of_add;
-
-     /*The to add the data and time automatically to all The Tables*/
-
-
-     -- This is my testing table and should not be sent into production
---  CREATE TABLE test_gender (
--- secrete_id BIGSERIAL PRIMARY KEY,
--- gender VARCHAR(50) 
--- );
-
-
 
 -- TEMPORARY TABLE FOR TESTING PURPOSES
 -- SHOULD NOT BE CREATED IN PRODUCTION 
