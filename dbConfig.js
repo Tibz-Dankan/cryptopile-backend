daf131cfbdc4bb10f2c767f9c2708404c82e1168
Tibz-Dankan <tibesigwadankan@gmail.com>
2022-03-11 11:00:10 +0300

    Client of the pg library is now connecting to the heroku postgres database

--- dbConfig.js
+++ dbConfig.js
@@ -2,10 +2,11 @@ require("dotenv").config();
 
 // HEROKU PSQL DB CONFIGURATIONS
 // const { Pool } = require("pg");
-// //production here
-// const isProduction = process.env.NODE_ENV === "production";
-// const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
-// // // new pool object
+const pg = require("pg");
+const isProduction = process.env.NODE_ENV === "production";
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+//  // new pool object (USING POOL)
 // const pool = new Pool({
 //   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 //   // ssl: {
@@ -13,19 +14,14 @@ require("dotenv").config();
 //   // },
 // });
 
-// MICROSOFT AZURE PSQL CONFIGURATIONS
-const pg = require("pg");
-
-const config = {
-  host: process.env.DB_HOST, //"<your-db-server-name>.postgres.database.azure.com",
-  user: process.env.DB_USER, // "<your-db-username>",
-  password: process.env.DB_PASSWORD, // "<your-password>",
-  database: process.env.DB_DATABASE, //"<name-of-database>",
-  port: process.env.DB_PORT,
-  // ssl: true,
+const configForHerokuPostgres = {
+  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+  // ssl: {
+  //   rejectUnauthorized: false,
+  // },
 };
 
-const pool = new pg.Client(config);
+const pool = new pg.Client(configForHerokuPostgres); // for heroku platform
 pool.connect((err) => {
   if (err) {
     throw err;
@@ -33,5 +29,27 @@ pool.connect((err) => {
     console.log("Database successfully connected!");
   }
 });
+
+// // MICROSOFT AZURE PSQL CONFIGURATIONS
+// const pg = require("pg");
+
+// const configForAzurePostgres = {
+//   host: process.env.DB_HOST, //"<your-db-server-name>.postgres.database.azure.com",
+//   user: process.env.DB_USER, // "<your-db-username>",
+//   password: process.env.DB_PASSWORD, // "<your-password>",
+//   database: process.env.DB_DATABASE, //"<name-of-database>",
+//   port: process.env.DB_PORT,
+//   // ssl: true,
+// };
+
+// // USING CLIENT OBJECT
+// const pool = new pg.Client(config); // for azure cloud
+// pool.connect((err) => {
+//   if (err) {
+//     throw err;
+//   } else {
+//     console.log("Database successfully connected!");
+//   }
+// });
 
 module.exports = pool;
