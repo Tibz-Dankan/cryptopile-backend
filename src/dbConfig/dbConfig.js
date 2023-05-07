615658f61fe59c639f0a41c8ca217fa1ba27216e
Tibz-Dankan <tibesigwadankan@gmail.com>
2023-05-07 15:53:32 +0300

    refactor: connect to varcel postgres

--- src/dbConfig/dbConfig.js
+++ src/dbConfig/dbConfig.js
@@ -1,31 +1,13 @@
 require("dotenv").config();
 const pg = require("pg");
-const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-let credentialObject;
+const db = new pg.Client({
+  connectionString: process.env.POSTGRES_URL,
+});
 
-const credentialObjForLocalDev = {
-  connectionString: connectionString,
-};
-
-const credentialObjForProd = {
-  connectionString: process.env.DATABASE_URL,
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
-};
-
-if (isProduction) {
-  credentialObject = credentialObjForProd;
-} else {
-  credentialObject = credentialObjForLocalDev;
-}
-
-const db = new pg.Client(credentialObject);
 db.connect((err) => {
   if (err) {
-    console.log("Error:Failed to connect to the database");
+    console.log(err, "Failed to connect to the database");
   } else {
     console.log("Database successfully connected!");
   }
